fd8874e4d094e76e42f1502f097474ed0c9d2805
mactavishkkk <isaiasleite230@gmail.com>
2022-09-03 09:46:11 -0400

    responsive-true

--- src/components/aboutContent/index.js
+++ src/components/aboutContent/index.js
@@ -19,13 +19,12 @@ const AboutContent = () => {
                     <img className="2xl:w-96 xl:w-96 md:w-[330px] absolute bambu02" src="/img/bambu-right.png" alt="Cartoon de um bambu com algumas folhas na lateral da página" />
                 </div>
             </section>
-
             <section>
-                <div className="container">
+                <div className="">
                     <div className="flex justify-center widthComponent" data-aos="fade-up" data-aos-duration="1000">
                         <img className="w-96" src="/img/title-about-me.png" alt="Título da seção com a imagem de um pergaminho oriental no centro da página" />
                     </div>
-                    <div className="2xl:mx-[0px] xl:mx-[0px] md:mx-[-120px] sm:mx-[-150px] marginComponent" data-aos="fade-in" data-aos-duration="2500" data-aos-delay="200">
+                    <div className="2xl:mx-[350px] xl:mx-[350px] md:mx-[250px] sm:mx-[-150px] divComponent" data-aos="fade-in" data-aos-duration="2500" data-aos-delay="200">
                         <p className="text-center lg:text-sm md:text-xs sm:text-xs font-mono ... subpixel-antialiased ... mb-3 shadow-lg text-justify">
                             Formado no nível técnico em tecnologia da informação, após mais ou menos 1 ano de experiência na área de suporte e infraestrutura foi quando descobrir o incrivel mundo do desenvolvimento. Desde então essa paixão foi se 'desenvolvendo' e foi ai que decidir migrar.
                             Atualmente com 8 meses de experiência em algumas tecnologias de base, pude trabalhar em atividades e projetos que envolveram a tecnologia, a pesquisa e a ciência, afim de conseguir minha primeira oportunidade na área venho me desenvolvendo em tecnologias JavaScript como: React, NodeJS e junto a isso alguns banco de dados SQL.
--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,6 +1,6 @@
 /* This example requires Tailwind CSS v2.0+ */
 import { Fragment } from 'react'
-import { Popover, Transition } from '@headlessui/react'
+import { Popover, Transition } from '@headlessui/react';
 import {
     MenuIcon,
     XIcon,
@@ -63,6 +63,7 @@ export default function Example() {
                                 <MenuIcon className="h-6 w-6" aria-hidden="true" />
                             </Popover.Button>
                         </div>
+                        
 
                         <Popover.Group as="nav" className="hidden md:flex space-x-10">
                             <a href="#experiences" className="text-base font-medium text-gray-500 hover:text-gray-900 font-mono ...">
