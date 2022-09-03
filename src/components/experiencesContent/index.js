1488d50b71fa2836e363d88bff703146f01498f7
mactavishkkk <isaiasleite230@gmail.com>
2022-09-03 12:28:48 -0400

    responsive-final

--- src/components/experiencesContent/index.js
+++ src/components/experiencesContent/index.js
@@ -27,7 +27,7 @@ const ExperienciesContent = () => {
                     <div className="flex justify-center mb-32 2xl:mb-32 xl:mb-32 lg:mb-3 md:mb-3 widthComponent" id="experiences" data-aos="fade-up" data-aos-duration="1000">
                         <img className="w-96" src="/img/title-experiences.png" alt="Titulo da seção de experiências, representado por um pergaminho" />
                     </div>
-                    <div className="shadow-lg 2xl:mx-[350px] xl:mx-[350px] md:mx-[250px] sm:mx-[-150px] divComponent formationComponent" data-aos="fade-left" data-aos-duration="1500" data-aos-delay="200">
+                    <div className="shadow-lg 2xl:mx-[350px] xl:mx-[350px] md:mx-[250px] sm:mx-[-150px] divComponent experiencesComponent" data-aos="fade-left" data-aos-duration="1500" data-aos-delay="200">
                         <h1 className="text-3xl font-bold font-mono ... subpixel-antialiased ...">Líder de Projeto</h1>
                         <h1 className="text-md font-mono ... subpixel-antialiased ... text-gray-600/75">Senac - Janeiro, 2022 - Março, 2022 | 03 meses</h1>
                         <p className="2xl:text-sm xl:text-sm lg:text-sm md:text-xs sm:text-xs font-mono ... subpixel-antialiased ... mt-3">
@@ -37,7 +37,7 @@ const ExperienciesContent = () => {
                             MySQL assim como boas noções em Git, GitHub e Metodologias ágeis.
                         </p>
                     </div>
-                    <div className="mt-6 shadow-lg 2xl:mx-[350px] xl:mx-[350px] md:mx-[250px] sm:mx-[-150px] divComponent formationComponent" data-aos="fade-left" data-aos-duration="1500" data-aos-delay="300">
+                    <div className="mt-6 shadow-lg 2xl:mx-[350px] xl:mx-[350px] md:mx-[250px] sm:mx-[-150px] divComponent experiencesComponent" data-aos="fade-left" data-aos-duration="1500" data-aos-delay="300">
                         <h1 className="text-3xl font-bold font-mono ... subpixel-antialiased ...">Suporte e Infraestrutura - TI</h1>
                         <h1 className="text-md font-mono ... subpixel-antialiased ... text-gray-600/75">Live Academia - Dezembro, 2021 - Março, 2022 | 04 meses</h1>
                         <p className="2xl:text-sm xl:text-sm lg:text-sm md:text-xs sm:text-xs font-mono ... subpixel-antialiased ... mt-3">
