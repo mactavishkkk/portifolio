21a959e392910d00ddcb338dcb2276e11f9b23be
mactavishkkk <isaiasleite230@gmail.com>
2022-09-16 16:51:07 -0400

    syle: alter at technologies section

--- src/components/technologiesContent/index.js
+++ src/components/technologiesContent/index.js
@@ -28,7 +28,7 @@ const TechnologiesContent = () => {
                         <img className="w-96" src="/img/title-technologies.png" alt="Titulo da seção de tecnologias, representado por um pergaminho" />
                     </div>
 
-                    <div className="2xl:mx-[450px] xl:mx-[500px] md:mx-[250px] sm:mx-[-150px] flex justify-around flex-wrap cardsComponent">
+                    <div className="2xl:mx-[450px] xl:mx-[350px] md:mx-[250px] sm:mx-[-150px] flex justify-around flex-wrap cardsComponent">
                         <div data-aos="zoom-in" data-aos-duration="1500" data-aos-delay="50">
                             <img className="w-20 mx-5 mb-10" src="/icons-img/html5-icon.png" alt="Ícone simbolizando a tecnologia HTML5" />
                         </div>
